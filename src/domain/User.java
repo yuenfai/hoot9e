330111ed149982e236dca5d0911d64f55eff6334
Yuen Fai <66880513+yuenfai@users.noreply.github.com>
2021-03-07 16:39:46 +0800

    Create User.java

---
+++ src/domain/User.java
@@ -0,0 +1,62 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package domain;
+
+import java.util.Date;
+
+/**
+ *
+ * @author Ruey
+ */
+public class User {
+
+    private int userID;
+    private String userDesc;
+    private Date DOB;
+    private Date dateCreated;
+
+    public User() {
+
+    }
+
+    public int getUserID() {
+        return userID;
+    }
+
+    public void setUserID(int userID) {
+        this.userID = userID;
+    }
+
+    public String getUserDesc() {
+        return userDesc;
+    }
+
+    public void setUserDesc(String userDesc) {
+        this.userDesc = userDesc;
+    }
+
+    public Date getDOB() {
+        return DOB;
+    }
+
+    public void setDOB(Date DOB) {
+        this.DOB = DOB;
+    }
+
+    public Date getDateCreated() {
+        return dateCreated;
+    }
+
+    public void setDateCreated(Date dateCreated) {
+        this.dateCreated = dateCreated;
+    }
+
+    @Override
+    public String toString() {
+        return "User{" + "userID=" + userID + ", userDesc=" + userDesc + ", DOB=" + DOB + ", dateCreated=" + dateCreated + '}';
+    }
+
+}
